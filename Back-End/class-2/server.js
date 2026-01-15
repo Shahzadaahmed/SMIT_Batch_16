a3b1cdce221bd734766c366c714c74fbd9fb3cb2
Talal <ta234567801@gmail.com>
2026-01-15 10:52:09 +0500

    Update and delete api created

--- Back-End/class-2/server.js
+++ Back-End/class-2/server.js
@@ -112,6 +112,9 @@ app.post("/user/save", async (req, res) => {
 
 // Note: Fetch all users from DB...!
 app.get("/users/fetch/all", async (req, res) => {
+  const { role } = req.query;
+  console.log("Role: ", role);
+
   try {
     const usersCount = await UserModal.countDocuments();
     console.log(`Counts: ${usersCount}`);
@@ -120,6 +123,16 @@ app.get("/users/fetch/all", async (req, res) => {
       return res.status(400).send({
         status: false,
         message: "No user found",
+      });
+    }
+
+    // 200 (Query filter)
+    if (role) {
+      const fetchUsers = await UserModal.find({ role });
+      return res.status(200).send({
+        status: true,
+        message: "Users fetched successfully",
+        data: fetchUsers,
       });
     }
 
@@ -136,6 +149,79 @@ app.get("/users/fetch/all", async (req, res) => {
     return res.status(500).send({
       status: false,
       message: "Something went wrong while fetching users",
+    });
+  }
+});
+
+// Note: Delete data from DB...!
+app.delete("/user/delete/:uid", async (req, res) => {
+  const { uid } = req.params;
+  console.log("Uid: ", uid);
+
+  try {
+    // 400
+    const checkUid = mongoose.Types.ObjectId.isValid(uid);
+    if (!checkUid) {
+      return res.status(400).send({
+        status: false,
+        message: "User id is required or Invalid user id",
+      });
+    }
+
+    // 200
+    const removeUser = await UserModal.findByIdAndDelete(uid);
+    if (removeUser) {
+      return res.status(200).send({
+        status: true,
+        message: "User deleted successfully",
+      });
+    }
+  } catch (error) {
+    // 500
+    console.log("Err while deleting data: ", error);
+    return res?.status(500).send({
+      status: false,
+      message: "Err while deleting user",
+    });
+  }
+});
+
+// Note: Update data from DB...!
+app.put("/user/update", async (req, res) => {
+  const { uid, updatedName, updatedAdd } = req.body;
+
+  try {
+    // 400
+    if (!uid || !updatedName || !updatedAdd) {
+      return res.status(400).send({
+        status: false,
+        message: "All fields are required",
+      });
+    }
+
+    // 200
+    const updateData = await UserModal.findByIdAndUpdate(
+      uid,
+      {
+        userName: updatedName,
+        address: updatedAdd,
+      },
+      { new: true }
+    );
+
+    if (updateData) {
+      return res.status(200).send({
+        status: true,
+        message: "User updated successfully",
+        data: updateData,
+      });
+    }
+  } catch (error) {
+    // 500
+    console.log("Err while updating data: ", error);
+    return res?.status(500).send({
+      status: false,
+      message: "Err while updating user",
     });
   }
 });
