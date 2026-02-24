d40af1d9c16dafb16f191628f9d13d044a480fc1
hasanCoder66 <52074642+Mohammad-Saad-Aly@users.noreply.github.com>
2026-02-24 08:30:25 +0500

    Multer lib integrated

--- Back-End/class-3/server.js
+++ Back-End/class-3/server.js
@@ -1,3 +1,92 @@
+// // Note: Main server file...!
+
+// // Importing libs...!
+// import express from "express";
+// import morgan from "morgan";
+// import cors from "cors";
+// import multer from "multer";
+// import fs from "fs";
+// import { config } from "dotenv";
+// import conectMongoDB from "./src/database/db.js";
+
+// import userRoutes from "./src/routes/user-routes/user-routes.js";
+
+// // Environment variables config...!
+// config({
+//   path: "./.env",
+// });
+
+// // Note: Database connection here...!
+// conectMongoDB();
+
+// if (!fs.existsSync("uploads")) {
+//   fs.mkdirSync("uploads");
+// }
+
+// // Global variables...!
+// const port = process.env.PORT;
+// const app = express();
+// // Saving file on their respective destination with file name...!
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, 'uploads/')
+//   },
+//   filename: (req, file, cb) => {
+//     console.log(`File: ${file}`);
+//     const uniqueFileName = Date.now() + '-' + file.originalname;
+//     cb(null, uniqueFileName);
+//   }
+// });
+// // Multer config...!
+// const upload = multer({
+//   storage: storage,
+//   limits: { fileSize: 5 * 1024 * 1024 }, // 5mb
+//   // fileFilter
+// });
+
+// // Middlewares...!
+// app.use(express.json());
+// app.use(morgan("dev"));
+// app.use(cors());
+
+// // app.use("/user", userRoutes);
+
+// app.post("/api/upload/file", upload.single('image'), (req, res) => {
+//   try {
+//     // 400
+//     if (!req?.file) {
+//       return res?.status(400).send({
+//         status: false,
+//         message: "File is required or No file receoved"
+//       });
+//     };
+
+//     // 200
+//     return res?.status(200).send({
+//       status: true,
+//       message: 'File uploaded succesfully'
+//     });
+//   }
+
+//   catch (error) {
+//     // 500
+//     console.log(`Something went wrong while saving image: ${error}`);
+//     return res?.status(500).send({
+//       status: false,
+//       message: "Internal server error"
+//     });
+//   }
+// });
+
+// // Server running...!
+// app.listen(port, () => {
+//   console.log(`Server is running on port: ${port}`);
+// });
+
+
+// <input accept="image/*" />
+
+
 // Note: Main server file...!
 
 // Importing libs...!
--- Tricky Test/app.js
+++ Tricky Test/app.js
@@ -5,3 +5,15 @@ const handleCheckBox = () => {
   if (checkBoxEl.checked) passwordEl.type = "text";
   else passwordEl.type = "password";
 };
+
+
+const user = {
+  userName: 'ahmed',
+  title: '',
+  city: "Karachi",
+  cuntry: "PK"
+}
+const condition = Object.values(user).some((item) => {
+  return item == ""
+})
+console.log(condition);
