d620f7934a5284f7f338b77df13da281b4028acd
hasanCoder66 <52074642+Mohammad-Saad-Aly@users.noreply.github.com>
2026-02-26 08:37:10 +0500

    Multer work finished

--- Back-End/class-3/server.js
+++ Back-End/class-3/server.js
@@ -1,10 +1,96 @@
-// // Note: Main server file...!
+// Note: Main server file...!
 
-// // Importing libs...!
+// Importing libs...!
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import multer from "multer";
+import fs from "fs";
+import path, { join } from "path";
+import { config } from "dotenv";
+import conectMongoDB from "./src/database/db.js";
+
+// const customPath = path.join('uploads', 'images', "img123.png");
+// console.log(`Path: ${customPath}`);
+
+// Environment variables config...!
+config({
+  path: "./.env",
+});
+
+// Note: Database connection here...!
+conectMongoDB();
+
+if (!fs.existsSync("uploads")) {
+  fs.mkdirSync("uploads");
+}
+
+// Global variables...!
+const port = process.env.PORT;
+const app = express();
+// Saving file on their respective destination with file name...!
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/')
+  },
+  filename: (req, file, cb) => {
+    console.log(`File: ${file}`);
+    const uniqueFileName = Date.now() + '-' + file.originalname;
+    cb(null, uniqueFileName);
+  }
+});
+// Multer config...!
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5mb
+});
+
+// Middlewares...!
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors());
+app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
+
+app.post("/api/upload/file", upload.single('image'), (req, res) => {
+  console.log(`File: ${JSON.stringify(req?.file)}`);
+  try {
+    // 400
+    if (!req?.file) {
+      return res?.status(400).send({
+        status: false,
+        message: "File is required or No file receoved"
+      });
+    };
+
+    // 200
+    return res?.status(200).send({
+      status: true,
+      message: 'File uploaded succesfully'
+    });
+  }
+
+  catch (error) {
+    // 500
+    console.log(`Something went wrong while saving image: ${error}`);
+    return res?.status(500).send({
+      status: false,
+      message: "Internal server error"
+    });
+  }
+});
+
+// Server running...!
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
+
+
+// Note: Main server file...!
+
+// Importing libs...!
 // import express from "express";
 // import morgan from "morgan";
 // import cors from "cors";
-// import multer from "multer";
 // import fs from "fs";
 // import { config } from "dotenv";
 // import conectMongoDB from "./src/database/db.js";
@@ -19,110 +105,22 @@
 // // Note: Database connection here...!
 // conectMongoDB();
 
+// // Global variables...!
+// const port = process.env.PORT;
+// const app = express();
+
 // if (!fs.existsSync("uploads")) {
 //   fs.mkdirSync("uploads");
 // }
 
-// // Global variables...!
-// const port = process.env.PORT;
-// const app = express();
-// // Saving file on their respective destination with file name...!
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, 'uploads/')
-//   },
-//   filename: (req, file, cb) => {
-//     console.log(`File: ${file}`);
-//     const uniqueFileName = Date.now() + '-' + file.originalname;
-//     cb(null, uniqueFileName);
-//   }
-// });
-// // Multer config...!
-// const upload = multer({
-//   storage: storage,
-//   limits: { fileSize: 5 * 1024 * 1024 }, // 5mb
-//   // fileFilter
-// });
-
 // // Middlewares...!
 // app.use(express.json());
 // app.use(morgan("dev"));
-// app.use(cors());
+// // app.use(cors());
 
-// // app.use("/user", userRoutes);
-
-// app.post("/api/upload/file", upload.single('image'), (req, res) => {
-//   try {
-//     // 400
-//     if (!req?.file) {
-//       return res?.status(400).send({
-//         status: false,
-//         message: "File is required or No file receoved"
-//       });
-//     };
-
-//     // 200
-//     return res?.status(200).send({
-//       status: true,
-//       message: 'File uploaded succesfully'
-//     });
-//   }
-
-//   catch (error) {
-//     // 500
-//     console.log(`Something went wrong while saving image: ${error}`);
-//     return res?.status(500).send({
-//       status: false,
-//       message: "Internal server error"
-//     });
-//   }
-// });
+// app.use("/user", userRoutes);
 
 // // Server running...!
 // app.listen(port, () => {
 //   console.log(`Server is running on port: ${port}`);
 // });
-
-
-// <input accept="image/*" />
-
-
-// Note: Main server file...!
-
-// Importing libs...!
-import express from "express";
-import morgan from "morgan";
-import cors from "cors";
-import fs from "fs";
-import { config } from "dotenv";
-import conectMongoDB from "./src/database/db.js";
-
-import userRoutes from "./src/routes/user-routes/user-routes.js";
-
-// Environment variables config...!
-config({
-  path: "./.env",
-});
-
-// Note: Database connection here...!
-conectMongoDB();
-
-// Global variables...!
-const port = process.env.PORT;
-const app = express();
-
-if (!fs.existsSync("uploads")) {
-  fs.mkdirSync("uploads");
-}
-
-// Middlewares...!
-app.use(express.json());
-app.use(morgan("dev"));
-// app.use(cors());
-
-app.use("/user", userRoutes);
-
-// Server running...!
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
-});
--- React-JS/my-app/src/screens/about/about.jsx
+++ React-JS/my-app/src/screens/about/about.jsx
@@ -1,23 +1,78 @@
 // Note: About page...!
 
-import React from 'react';
-import Header from '../../components/header/header';
-import fruits from '../../utils/data';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const About = () => {
+    const [imagefile, setImageFile] = useState(null);
+    const [preview, setPreview] = useState(null);
+
+    const handleChange = (e) => {
+        const file = e?.target?.files[0];
+        // console.log('Form event: ', file);
+
+        if (file) {
+            setImageFile(file);
+            setPreview(URL.createObjectURL(file));
+        };
+    };
+
+    const uploadImgToServer = async () => {
+        const apiUrl = "http://localhost:5000/api/upload/file";
+
+        try {
+            if (!imagefile) {
+                console.log('Image is required');
+                return
+            };
+            console.log('Image file: ', imagefile);
+
+            const formData = new FormData();
+            formData.append('image', imagefile);
+            console.log('Form data: ', formData);
+
+            const res = await axios.post(
+                apiUrl,
+                formData,
+                {
+                    headers: {
+                        "Content-Type": "multipart/form-data",
+                    },
+                },
+            );
+            console.log('Api res: ' , res);
+        }
+
+        catch (error) {
+            console.log('Something went wrong while uploading image to server: ', error);
+        }
+    }
+
+    // useEffect(() => {
+    //     preview && console.log(preview);
+    // }, [preview]);
+
     return (
         <div>
-            <Header title='about' />
+            <h1> Image uploading React JS with Node JS </h1>
 
-            <ul>
-                {
-                    fruits.map((item, index) => {
-                        return (
-                            <li key={index}> {`${index + 1} - ${item}`} </li>
-                        )
-                    })
-                }
-            </ul>
+            <input
+                type="file"
+                accept="image/*"
+                onChange={handleChange}
+            />
+
+            {
+                preview &&
+                <img
+                    src={preview}
+                    alt="File"
+                    height={200}
+                    width={200}
+                />
+            }
+            <br /> <br /> <br />
+            <button onClick={uploadImgToServer}> Upload Image </button>
         </div>
     )
 }
